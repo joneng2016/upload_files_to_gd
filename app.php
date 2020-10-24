26a1d81a32cdccf30f743bb30327abe975a2e7b0
joneng2016 <jonathan_samara@hotmail.com>
2020-10-24 11:04:21 -0200

    upload remove vendor

--- app.php
+++ app.php
@@ -5,7 +5,10 @@ require "vendor/autoload.php";
 use App\Log\Log as log;
 
 try {
-    log::info("start application");    
+    
+    log::info("start application");
+
+    log::info("read .env file");
 } catch (\Throwable $th) {
     log::error($th);
 }
