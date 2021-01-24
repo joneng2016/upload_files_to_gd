064a843599f442c30cebb3111debdf08d68c578e
joneng2016 <jonathan_samara@hotmail.com>
2021-01-24 14:46:37 -0200

    subindo alteracoe no arquivo app que foram esquecidas no ultimo commit

--- app.php
+++ app.php
@@ -5,19 +5,28 @@ require "helper/helper.php";
 
 use App\Artefacts\TextosDaAplicacao as texts;
 use App\Factory\FactoryService;
+
 use App\Bootstrap\Bootstrap;
+use App\Bootstrap\GlobalSet;
+
 use App\Log\Log as log;
 
 $text = texts::start();
+
 $factoryService = FactoryService::start();
 
 log::start()->info($text->startApplication);
 log::start()->info($text->readEnvFile);
 
-Bootstrap::run();
+$bootstrap = Bootstrap::start();
+$globalSet = GlobalSet::start();
+
+$bootstrap->run();
+$globalSet->run();
 
 $googleDriveApiConnection = $factoryService->googleApiService();
 
 $googleDriveApiConnection->startConnection();
 $googleDriveApiConnection->loadDataThatHappensUpload();
-$googleDriveApiConnection->buildFileWithRelationThatIsOkTrait();
+$googleDriveApiConnection->buildFileWithRelationThatIsOk();
+$googleDriveApiConnection->uploadFileGoogleApi();
