4e6e5236cf6bc440ce25cbf1cc3e74e6bae88a9f
joneng2016 <jonathan_samara@hotmail.com>
2021-01-30 16:48:40 -0200

    mai dificil j foi, agora eh oh a mordomia

--- app/Service/TraitGoogleApiService/StartConnectionTrait.php
+++ app/Service/TraitGoogleApiService/StartConnectionTrait.php
@@ -38,17 +38,17 @@ trait StartConnectionTrait {
 
             } else {
 
-//                $authUrl = $this->context->client->createAuthUrl();
-//                $addressOfFile = write_url_to_access($authUrl);
+                $authUrl = $this->context->client->createAuthUrl();
+                $addressOfFile = write_url_to_access($authUrl);
                 
-//                $this->log->info($this->text->textoAcessoArquivoUrl($addressOfFile));
+                $this->log->info($this->text->textoAcessoArquivoUrl($addressOfFile));
 
-//                fgets(STDIN);
+                fgets(STDIN);
 
-//                $authCode = read_file($this->tokenPath);
-//                $accessToken = $this->context->client->fetchAccessTokenWithAuthCode($authCode);
+                $authCode = read_file($this->tokenPath);
+                $accessToken = $this->context->client->fetchAccessTokenWithAuthCode($authCode);
                 
-//                $this->context->client->setAccessToken($accessToken);
+                $this->context->client->setAccessToken($accessToken);
             }
  
             if (!file_exists(dirname($this->tokenPath)))
--- app/Service/TraitGoogleApiService/UploadFileGoogleApiTrait.php
+++ app/Service/TraitGoogleApiService/UploadFileGoogleApiTrait.php
@@ -2,12 +2,36 @@
 
 namespace App\Service\TraitGoogleApiService;
 
+use Google_Service_Drive_DriveFile;
+use Google_Service_Drive;
+
 trait UploadFileGoogleApiTrait {
 
 	public function uploadFileGoogleApi() {
 
-		$this->context->fileProcess->mapMySelf(function($address){
-			dd($address);
+		$this->context->service = new Google_Service_Drive($this->context->client);
+
+		$this->context->fileProcess->mapMySelf(function($address){	
+
+			$posName = explode("\\",$address);			
+			$name = $posName[sizeof($posName)-1];
+			
+
+			$file = new Google_Service_Drive_DriveFile($this->context->client);			
+			$file->setName("{$name}");
+			$file->setParents(["1yITaW-C_ECskm-n6fXOQAiP5IwfPdMpp"]);
+
+			$this->context->service->files->create(
+				$file,
+				[
+					"data" => file_get_contents($address),
+					"mimeType" => "application/octet-stream",
+					"uploadType" => "multipart"
+				]
+			);
+
+			dd("*");
+
 		});
 
 	}
